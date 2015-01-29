61f8fb54ca57b3c1f4918e8f85a1443d8c2228e1
unknown <lillo14108@iesmarenostrum.com>
2015-01-29 20:07:55 +0100

    Parte servidor peliculas

--- api/movies.php
+++ api/movies.php
@@ -3,7 +3,7 @@
 // Modelo de objetos que se corresponde con la tabla de MySQL
 class Movie extends \Illuminate\Database\Eloquent\Model
 {
-
+	public $timestamps = false;
 }
 
 /* Obtención de la lista de películas */
@@ -54,6 +54,87 @@ $app->get('/movies/:name', function ($name) use ($app) {
   // Mostramos la vista
   $app->render('movie_template.php'); 
 
+});
+
+/*  Borrado de una película en concreto  */
+$app->delete('/movies/:name', function ($name) use ($app) {
+
+  // Creamos un objeto collection + json con la película pasada como parámetro
+
+  // Obtenemos el objeto request, que representa la petición HTTP
+  $req = $app->request;
+
+  // Obtenemos la ruta absoluta de este recurso
+  $absUrl =  $req->getScheme() . "://" . $req->getHost() . $req->getRootUri() . $req->getResourceUri();
+
+  // Obtenemos la película de la base de datos a partir de su id y la convertimos del formato Json (el devuelto por Eloquent) a un array PHP
+  $p = \Movie::find($name);  
+  $p->delete();
+
+  // Mostramos la vista
+  $app->render('movie_template.php'); 
+
+});
+
+/*  Insertar una película en concreto  */
+$app->post('/movies', function () use ($app) {
+
+  // Creamos un objeto collection + json con la película pasada como parámetro
+
+  // Obtenemos el objeto request, que representa la petición HTTP
+  $req = $app->request;
+
+  // Obtenemos la ruta absoluta de este recurso
+  //$absUrl =  $req->getScheme() . "://" . $req->getHost() . $req->getRootUri() . $req->getResourceUri();
+
+  // Obtenemos la película de la base de datos a partir de su id y la convertimos del formato Json (el devuelto por Eloquent) a un array PHP
+  
+	$movie = new Movie;
+	
+	$body = $app->request->getBody();
+	$array = json_decode($body, true);
+	
+	$movie->name = $array['template']['data'][0]['value'];
+	$movie->description = $array['template']['data'][1]['value'];
+	$movie->director = $array['template']['data'][2]['value'];
+	$movie->datePublished = $array['template']['data'][3]['value'];
+	$movie->embedUrl = $array['template']['data'][4]['value'];
+	
+	$movie->save();
+
+  // Mostramos la vista
+  $app->render('movie_template.php'); 
+
+});
+
+/*  Actualizar una película en concreto  */
+$app->put('/movies/:name', function ($name) use ($app) {
+
+  // Creamos un objeto collection + json con la película pasada como parámetro
+
+  // Obtenemos el objeto request, que representa la petición HTTP
+  $req = $app->request;
+
+  // Obtenemos la ruta absoluta de este recurso
+  $absUrl =  $req->getScheme() . "://" . $req->getHost() . $req->getRootUri() . $req->getResourceUri();
+
+  // Obtenemos la película de la base de datos a partir de su id y la convertimos del formato Json (el devuelto por Eloquent) a un array PHP
+  
+	$movie = \Movie::find($name);
+	
+	$body = $app->request->getBody();
+	$array = json_decode($body, true);
+	
+	$movie->name = $array['template']['data'][0]['value'];
+	$movie->description = $array['template']['data'][1]['value'];
+	$movie->director = $array['template']['data'][2]['value'];
+	$movie->datePublished = $array['template']['data'][3]['value'];
+	$movie->embedUrl = $array['template']['data'][4]['value'];
+	
+	$movie->save();
+
+  // Mostramos la vista
+  $app->render('movie_template.php'); 
 
 });
 
